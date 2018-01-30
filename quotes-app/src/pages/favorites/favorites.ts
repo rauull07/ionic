11b7232d263cf6d67ed35c6033b4690a0814ba66
raul.crisan <Raul.Crisan@isdc.eu>
2018-01-30 16:56:37 +0200

    first commit on qoutes-app

---
+++ quotes-app/src/pages/favorites/favorites.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ModalController } from 'ionic-angular';
+import { QuotesService } from '../../services/quotes';
+import { Quote } from '../../data/quote.interface';
+import { QuotePage } from '../quote/quote';
+
+@Component({
+  selector: 'page-favorites',
+  templateUrl: 'favorites.html',
+})
+export class FavoritesPage {
+  quotes: Quote[];
+
+  constructor(
+    private quotesService: QuotesService,
+    private modalCtrl: ModalController) {
+  }
+
+  ionViewWillEnter() {
+    this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage, quote);
+    modal.present();
+    modal.onDidDismiss((remove: boolean) => {
+      if (remove) {
+        this.onRemoveFromFavorites(quote);
+      }
+    });
+  }
+
+  onRemoveFromFavorites(quote: Quote) {
+    this.quotesService.removeQuoteFromFavorites(quote);
+    //this.quotes = this.quotesService.getFavoriteQuotes();
+    this.quotes.splice(this.quotes.indexOf(quote), 1);
+  }
+
+}
---
+++ quotes-app/src/pages/quotes/quotes.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+import { QuotesService } from '../../services/quotes';
+
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit {
+
+  quoteGroup : {category: string, quotes: Quote[], icon: string};
+
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController,
+    private quotesService: QuotesService){}
+
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  //   // Add elvis operator (?) in template to use this approach
+  // }
+
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorites(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+         text: 'Yes, go ahead',
+          handler: () => {
+           this.quotesService.addQuoteToFavorites(selectedQuote);
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Canceled!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  onRemoveFromFavorites(selectedQuote: Quote) {
+    this.quotesService.removeQuoteFromFavorites(selectedQuote);
+  }
+
+  isFavorite(quote: Quote) {
+    return this.quotesService.isQuoteFavorite(quote);
+  }
+}
