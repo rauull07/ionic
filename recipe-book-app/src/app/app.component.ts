a84219120bad3db708b8bcb4e28b63eca9c394ef
raul.crisan <Raul.Crisan@isdc.eu>
2018-02-01 16:45:53 +0200

    [recipe-book] - logout

--- recipe-book-app/src/app/app.component.ts
+++ recipe-book-app/src/app/app.component.ts
@@ -7,19 +7,25 @@ import * as firebase from 'firebase';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from '../pages/signin/signin';
 import { SignupPage } from '../pages/signup/signup';
+import { AuthService } from '../services/auth';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage:any = TabsPage;
+  rootPage:any = TabsPage;
   signinPage = SigninPage;
   signupPage = SignupPage;
   isAuthenticated = false;
 
   @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private menuCtrl: MenuController,
+              private authService: AuthService) {
+
     firebase.initializeApp({
       apiKey: "AIzaSyBhitlMDUj0RPuF0_zW5P2BA1CzmwAkQ_M",
       authDomain: "ionic-recipe-book-9f62a.firebaseapp.com"
@@ -28,10 +34,10 @@ export class MyApp {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.isAuthenticated = true;
-        this.nav.setRoot(this.tabsPage);
+        this.rootPage = TabsPage;
       } else {
         this.isAuthenticated = false;
-        this.nav.setRoot(this.signinPage);
+        this.rootPage = SigninPage;
       }
     });
 
@@ -49,7 +55,9 @@ export class MyApp {
   }
 
   onLogout() {
-
+    this.authService.logout();
+    this.menuCtrl.close();
+    this.nav.setRoot(SigninPage);
   }
 }
 
