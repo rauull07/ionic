cacff923993f89031537f89ba9e6ae6249922103
raul.crisan <Raul.Crisan@isdc.eu>
2018-02-01 10:13:28 +0200

    [recipe-book] - added form template driven

---
+++ recipe-book-app/src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+
+  onAddItem(form: NgForm) {
+    console.log(form);
+  }
+}
