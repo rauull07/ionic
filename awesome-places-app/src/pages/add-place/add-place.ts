58f7197374283cbd453e0bb01301e26238b9ea1b
raul.crisan <Raul.Crisan@isdc.eu>
2018-02-02 11:53:18 +0200

    [awesome-places] - created a basic form

--- awesome-places-app/src/pages/add-place/add-place.ts
+++ awesome-places-app/src/pages/add-place/add-place.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddPlacePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NgForm } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -18,8 +12,7 @@ export class AddPlacePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPlacePage');
+  onSubmit(form: NgForm) {
+    console.log(form.value);
   }
-
 }
